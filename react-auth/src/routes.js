1bfe6850a7f0fc21a50316bd7d98b0a559c8080e
RohdeMG <rohde1392@gmail.com>
2020-07-27 15:07:37 +0100

    ok

---
+++ react-auth/src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={()} />
+        </Switch>
+    </BrowserRouter>
+);
